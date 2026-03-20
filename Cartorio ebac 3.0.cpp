89a88e6f6add662dc98e8c0d0d6dfb0a25611736
ao816247-ux <ao816247@gmail.com>
2026-03-20 18:57:50 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usúario

--- Cartorio ebac 3.0.cpp
+++ Cartorio ebac 3.0.cpp
@@ -112,6 +112,7 @@ int main()
     printf("\t1 - Registrar nomes\n");
     printf("\t2 - Consultar nomes\n");
     printf("\t3 - Deletar nomes\n\n");
+    printf("\t4 - Sair do sistema\n\n");
     printf("Opção: ");//fim do menu
     
 	scanf("%d", &opcao); // inicio da selecao
@@ -131,6 +132,12 @@ int main()
 	
 	case 3:
 	Deletar();
+   	break;
+   	
+   	
+   	case 4:
+   	printf("Obrigado por utilizar o sistema!\n");
+   	return 0;
    	break;
    	
     
